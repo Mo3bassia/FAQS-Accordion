d2446c2775a08da3e6af08094555a1a91c1e55db
Mohamed Abassia <mo3bassia@gmail.com>
2024-09-17 08:04:31 +0300

    Initial Commit => All project has been uploaded

---
+++ README.md
@@ -0,0 +1,7 @@
+# Frontend Mentor - FAQs accordion
+
+- This is a simple design for FAQs accordion challenge by Frontend Mentor Website
+
+## Live Demo
+
+- [Live Preview Here](https://faqs-accordion-mo3bassias-projects.vercel.app)
---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+let listQuestions = document.querySelector(".questions-list");
+
+// Make first open automaticlly after remove class hidden in HTML
+function makeOpen(i) {
+  listQuestions
+    .querySelector(`li:nth-of-type(${i}) .toggle`)
+    .classList.remove("hidden");
+  listQuestions.querySelector(`li:nth-of-type(${i}) .toggle`).style.height = `${
+    listQuestions.querySelector("li:first-of-type .toggle").scrollHeight
+  }px`;
+}
+makeOpen(1);
+
+listQuestions.querySelectorAll("li").forEach((li) => {
+  li.querySelector(".content").onclick = function () {
+    li.querySelector(".toggle").classList.toggle("hidden");
+    if (!li.querySelector(".toggle").classList.contains("hidden")) {
+      li.querySelector("img").src = "assets/images/icon-minus.svg";
+      li.querySelector(".toggle").style.height = `${
+        li.querySelector(".toggle").scrollHeight
+      }px`;
+    } else {
+      li.querySelector("img").src = "assets/images/icon-plus.svg";
+      li.querySelector(".toggle").style.height = "0px";
+    }
+  };
+});
